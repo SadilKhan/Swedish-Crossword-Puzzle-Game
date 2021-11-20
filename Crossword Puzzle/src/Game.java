8f3deccceace54c299b72b18ddfc5813fe0dd4cf
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2021-11-20 21:27:30 +0100

    update and bug fix
    - Added more comments
    - Fixed an issue with the bottom gray boxes where multiple keyboards are being generated upon clicking different boxes.
    - Moved some utility methods to a new class

--- Crossword Puzzle/src/Game.java
+++ Crossword Puzzle/src/Game.java
@@ -4,49 +4,40 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Game implements FocusListener{
-	private int row;
-	private int column;
-	private String[][] field;
-	private ArrayList<String> acrossClues;
-	private ArrayList<String> downClues;
-	OnScreenKeyboard curKeyboard;
-	JFrame gameFrame;
-	TextBox curTextBox;
-	ArrayList<ArrayList<TextBox>> textBoxes;
-	ArrayList<JLabel> allLabels;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Game game=new Game();
-
-	}
+public class Game implements ActionListener, FocusListener{
+	/* Class for Main Puzzle Game */
+	
+	private int row; // Number of Rows in the Playing Field.
+	private int column; // Number of Columns in the Playing Field.
+	private String[][] field; // The Information about the different types of squares.
+	private ArrayList<String> acrossClues; // The clues for horizontal box fields.
+	private ArrayList<String> downClues; // The clues for vertical box fields.
+	OnScreenKeyboard curKeyboard; // Current Keyboard associated with the text field.
+	JFrame gameFrame; // Main Game JFrame
+	ArrayList<ArrayList<TextBox>> textBoxes; // All the text boxes in the playing field.
+	ArrayList<JLabel> allLabels; // All the labels in the clues panel.
+	int totalGraySquare=0; // Total number of Gray Squares.
+	JButton checkButton; // Button for checking.
 	
 	Game(int r, int c, String[][] f,ArrayList<String> a,ArrayList<String> d) {
+		/* Constructor */
 		row=r;
 		column=c;
 		field=f;
 		acrossClues=a;
 		downClues=d;
 		
-		// Find the Hint Keys
+		// Start the UI
 		
 		gameUI();
 		
-	}
-	Game(){
-		gameUI();
 	}
 	
 	public void gameUI() {
-		// Frame
-		gameFrame=new JFrame("Swedish-Style Crossword Puzzle");
-		gameFrame.setSize(1200,1000);
-		gameFrame.setResizable(false);
-		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gameFrame.setLayout(null);
-		gameFrame.setLocationRelativeTo(null);
+		/* The Interface for the game*/
+		
+		// Create Frame
+		createGameFrame();
 		
 		// Create the Playing Field
 		createField();
@@ -54,8 +45,9 @@ public class Game implements FocusListener{
 		// Create the Statement Field
 		createStatement();
 		
-		// Create the Check Field
+		// Create the Check Field. Contains the final gray squares and the check button
 		createCheck();
+		
 		
 		
 		gameFrame.setVisible(true);
@@ -63,57 +55,113 @@ public class Game implements FocusListener{
 		
 		
 	}
+	
+	private void createGameFrame() {
+		/* Create a JFrame upon which we will add component for UI */
+		
+		gameFrame=new JFrame("Swedish-Style Crossword Puzzle");
+		gameFrame.setSize(1000,800); // Set the size of the frame
+		gameFrame.setResizable(false); // gameFrame isn't resizable.
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setLayout(null);
+		gameFrame.setLocationRelativeTo(null);// Makes the frame appear in the center of the screen.
+	}
 	private void createStatement() {
 		
+		/* Creates the left panel for clues . It contains one Main Panel and three sub Panels added to 
+		 the main Panel. The three sub-panels are firstPanel(which contains only one label) and acrossPanel
+		 for across clues and downPanel for down clues
+		 */
+		
+		// Main Panel
 		JPanel statementPanel=new JPanel();
 		statementPanel.setLayout(new BoxLayout(statementPanel,BoxLayout.Y_AXIS));
 		statementPanel.setBounds(10,10,250,700);
 		statementPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		//statementPanel.setBackground(Color.red);
 		
-		// Labels
-		JPanel firstPanel=new JPanel(new BorderLayout());
+		// The First Panel which contains only one label.
+		JPanel firstPanel=new JPanel();
 		JLabel firstLabel=new JLabel("Crossword Puzzle Instructions:");
-		firstLabel.setForeground(new Color(76,0,153));
-		firstLabel.setFont(new Font("Arial",Font.BOLD,15));
+		firstLabel.setForeground(new Color(76,0,153));// Change the color of the label
+		firstLabel.setFont(new Font("Arial",Font.BOLD,15)); // Change the font style of the label
 		firstPanel.add(firstLabel);
 		//allLabels.add(firstLabel);
 		
+		// Across Panel for across statements
 		JPanel acrossPanel=new Panel(acrossClues).aPanel;
+		// Down Panel for down statements
 		JPanel downPanel=new Panel(downClues).aPanel;
 		
-		
+		// Add the sub-panels in the Main Panel statementPanel
 		statementPanel.add(firstPanel);
 		statementPanel.add(acrossPanel);
 		statementPanel.add(downPanel);
 		
 		
-		
+		// Add the Main Panel in the main Frame gameFrame
 		gameFrame.add(statementPanel);
 
 		
 	}
 	
 	private void createCheck() {
-		JPanel checkPanel=new JPanel();
-		checkPanel.setBounds(260,700,740,100);
-		checkPanel.setBackground(Color.black);
+		/* Creates the bottom gray squares and check button. It has one Main Panel and two panels. One for the 
+		 squares and one for the button */
 		
-		gameFrame.add(checkPanel);
+		//Main Panel
+		JPanel mainPanel=new JPanel(new FlowLayout());
+		mainPanel.setBounds(340,700,660,50);
+		
+		// Sub-Panel for gray squares
+		JPanel checkPanel=new JPanel(new GridLayout(1,totalGraySquare+1));
+		
+		// Number of Gray squares is saved in the totalGraySquare. Add that many gray squares.
+		ArrayList<TextBox> boxes=new ArrayList<TextBox>();
+		for(int i=0;i<totalGraySquare;i++) {
+			TextBox tb=new TextBox();
+			tb.textBox.setPreferredSize(new Dimension(50,50));
+			tb.textBox.setBackground(Color.gray);
+			// When a text box is selected, we need to dispose the other keyboard
+			tb.textBox.addFocusListener(this); 
+			// Add text boxes in the sub-panel for gray boxes
+			checkPanel.add(tb.textBox);
+			boxes.add(tb);
+			
+		}
+		// Add the text field to textBoxes variable.
+		textBoxes.add(boxes);
+		
+		// The sub panel for the button.
+		JPanel checkButtonPanel= new JPanel(new FlowLayout(FlowLayout.CENTER));
+		checkButton=new JButton("Check"); // The check button
+		checkButton.setFocusable(false); // Gets rid of the border
+		checkButton.setSize(10,10);
+		checkButton.addActionListener(this);
+		checkButtonPanel.add(checkButton);
+		checkPanel.add(checkButtonPanel);
+		
+		// Add the sub-panels to the main panels.
+		mainPanel.add(checkPanel);
+		mainPanel.add(checkButtonPanel);
+		// Add the main panel to the main game frame.
+		gameFrame.add(mainPanel);
 
 		
 	}
 	
 	private void createField() {
 		
-		// Create the Playing field
+		/* Creates the Playing field. Contains one Main Panel and row x Column numbers of text fields with
+		 modified on-screen keyboard attached with each one of them.
+		  */
 		
-		// Panel
+		// Main Panel
 		JPanel fieldPanel=new JPanel(new GridLayout(row,column));
 		fieldPanel.setBounds(260,5,740,695);
 		
 		
-		// Add textfield in the fieldPanel
+		// Add row x column textfields in the fieldPanel
 		textBoxes=new ArrayList<ArrayList<TextBox>>();
 		
 		for(int i=0;i<row;i++) {
@@ -121,37 +169,47 @@ public class Game implements FocusListener{
 			ArrayList<TextBox> boxes=new ArrayList<TextBox>();
 			for(int j=0;j<column;j++) {
 				TextBox tb;
-				
+				/* We have 4 types of boxes.
+				 O --> Normal boxes ( All the keys in the keyboard is enabled)
+				 H --> Help Boxes ( The keyboard has only four random keys and the answer key enabled)
+				 X --> Black Boxes (No keyboard)
+				 S --> Special Boxes(All the keys in the keyboard is enabled)
+				 *  */
 				if (field[i][j].contains("H")) {
-					// Hint Boxes
-					char[] hintKeys=getRandomKeys(field[i][j].charAt(2));
-					tb=new TextBox(hintKeys);
+					/* Help Boxes*/
+					//Generate an array with four random keys and the answer key. Only these keys will be enabled.
+					char[] hintKeys=GameUtil.getRandomKeys(field[i][j].charAt(2));
+					tb=new TextBox(hintKeys);// Create an object of TextBox object with modified keyboard.
 					tb.textBox.setBackground(new Color(37,150,190));
-					tb.textBox.addFocusListener(this);
+					tb.textBox.addFocusListener(this);// When a text box is selected, dispose the other keyboard
 				}else if(field[i][j].contains("X")) {
-					// Black Boxes
+					/* Black Boxes. Contains no keyboard*/
 					tb=new TextBox();
 					tb.textBox.setBackground(Color.black);
-					tb.textBox.setEnabled(false);
+					tb.textBox.setEnabled(false);// Disable the text field
 					tb.textBoxEnabled=false;
-					tb.textBox.removeMouseListener(tb);
+					tb.textBox.removeMouseListener(tb); // mouse click on the black boxes will be ignored
 				}else if(field[i][j].contains("S")) {
-					// Special Boxes
+					/*Special Boxes*/
 					tb=new TextBox();
 					tb.textBox.setBackground(Color.gray);
 					tb.textBox.addFocusListener(this);
+					totalGraySquare+=1; // When a gray box is created, increase the number of gray squares
 				}else {
-					// Normal Boxes
-					
+					/*Normal Boxes*/
 					tb=new TextBox();
 					tb.textBox.addFocusListener(this);
 				}
 				
+				// boxes is an array which contains the textboxes for every row.
 				boxes.add(tb);
-				// Add the textBox in the panel
+				/* After the text box type is identified and object of that type is created ,
+				 * add the textBox to the panel
+				 */
 				fieldPanel.add(boxes.get(j).textBox);
 				
 			}
+			// textBoxes variable contains all the text boxes.
 			textBoxes.add(boxes);
 			
 		}
@@ -163,48 +221,25 @@ public class Game implements FocusListener{
 	}
 	
 	
-
-	private char[] getRandomKeys(char c) {
-		ArrayList<Character> hintKeys=new ArrayList<Character>();
-		hintKeys.add(c);
-		String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-		
-		
-		while (hintKeys.size()<5) {
-			Random rd= new Random();
-			char letter = abc.charAt(rd.nextInt(abc.length()));
-			if (letter!=c) {
-				hintKeys.add(letter);
-				
-			}
-			
-		}
-		
-		// Transform the ArrayList to Array
-		char[] hintKeyArrays=new char[hintKeys.size()];
-		
-		for(int i=0;i<hintKeys.size();i++) {
-			hintKeyArrays[i]=hintKeys.get(i);
-		}
-		return hintKeyArrays;
-		}
-
 	@Override
 	public void focusGained(FocusEvent e) {
-		// TODO Auto-generated method stub
-		
-		for(int i=0;i<row;i++) {
+		// When a text box is selected, this method is called.
+		outerloop:
+		for(int i=0;i<textBoxes.size();i++) {
+			ArrayList<TextBox> temptextBoxes=textBoxes.get(i);
 			
-			for(int j=0;j<column;j++) { 
+			for(int j=0;j<temptextBoxes.size();j++) { 
 				//System.out.println(e.getSource());
 				
-				if(e.getSource()==textBoxes.get(i).get(j).textBox) {
+				if(e.getSource()==temptextBoxes.get(j).textBox) {
+					// If there is a keyboard on the screen, it disposes
 					if(curKeyboard!=null) {
 						curKeyboard.dispose();
-						curKeyboard=textBoxes.get(i).get(j).kyb;
+						curKeyboard=temptextBoxes.get(j).kyb; // Assign a new keyboard
+						break outerloop;
 					}else {
-						curKeyboard=textBoxes.get(i).get(j).kyb;
+						curKeyboard=temptextBoxes.get(j).kyb;
+						break outerloop;
 						
 					}
 				}
@@ -217,6 +252,13 @@ public class Game implements FocusListener{
 	@Override
 	public void focusLost(FocusEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 		
 	}
 	
