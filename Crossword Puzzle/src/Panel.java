d20e3edb04ad0b597223f8202d63bb74878c47bf
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2021-11-20 22:09:03 +0100

    added more comments

--- Crossword Puzzle/src/Panel.java
+++ Crossword Puzzle/src/Panel.java
@@ -1,19 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.util.*;
 
 public class Panel {
+	/* Custom Class to create Panels for Across Statement and Down Statement */
 	
-	ArrayList<String> clues;
-	JPanel aPanel;
-
-	public static void main(String[] args) {
-		
-
-	}
+	ArrayList<String> clues; // Contains all the clues for Across or Down
+	JPanel aPanel; // The Main Panel
 	
 	Panel(ArrayList<String> lst ){
+		// Constructor
 		clues=lst;
 		createPanel();
 		
@@ -23,38 +19,41 @@ public class Panel {
 		
 		// Panel for Across clues and Down Clues
 		aPanel=new JPanel();
-		aPanel.setLayout(new BoxLayout(aPanel,BoxLayout.Y_AXIS));
+		aPanel.setLayout(new BoxLayout(aPanel,BoxLayout.Y_AXIS)); // Box Layout is used for vertical positioning of labels
 		
-		
+		// Add the clues in the aPanel
 		for(int i=0;i<clues.size();i++) {
+			/* Every item in the clues except the first item is of the form 
+			 * n.clue where n is a number which is colored differently than clue.
+			 * The First item is either Across or Down.
+			 * A panel is created for every item.
+			 * So, the item is split and positioned separately in a panel.
+			 * To efficiently position the two strings, borderlayout is used.
+			 * 
+			 */
 			String[] splitClues=clues.get(i).split("\\.",-1);
+			// Add a dot to the number. n--> n.
+			if(splitClues.length>1) {splitClues[0]=splitClues[0]+".";};
+			// For every item in the Across/Down lists, a panel is created
 			JPanel tempPanel=new JPanel(new BorderLayout());
 			
 			for (int j=0;j<splitClues.length;j++) {
-				JLabel tempLabel=new JLabel();
-				//tempLabel.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+				JLabel tempLabel=new JLabel(splitClues[j]);
 				
 				if(j==0) {
+					// Change the color for Across word and the bullets
 					tempLabel.setForeground(new Color(76,0,153));
-					if (splitClues.length==1) {
-						tempLabel.setText(splitClues[j]);
-						tempLabel.setFont(new Font("Arial",Font.BOLD,12));
-					}else {
-						tempLabel.setText(splitClues[j]+".");
-						tempLabel.setFont(new Font("Arial",Font.BOLD,10));
-					}
-					
+					tempLabel.setFont(new Font("Arial",Font.BOLD,12));
 					tempPanel.add(tempLabel,BorderLayout.WEST);
 				}
 				else {
-					tempLabel.setText(splitClues[j]);
-					tempLabel.setFont(new Font("Arial",Font.PLAIN,10));
+					tempLabel.setFont(new Font("Arial",Font.PLAIN,12));
 					tempPanel.add(tempLabel,BorderLayout.CENTER);
 				}
-				
-					
-				
+
 			}
+			
+			// Add each clue statements in the main panels
 			aPanel.add(tempPanel);
 		}
 		
