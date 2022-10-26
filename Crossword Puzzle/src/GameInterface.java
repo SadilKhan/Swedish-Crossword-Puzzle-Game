41e2b491be950473133596ecee98368de9bf60b6
Mohammad Sadil Khan <45021067+SadilKhan@users.noreply.github.com>
2022-10-26 17:17:58 +0200

    Delete Crossword Puzzle directory

--- Crossword Puzzle/src/GameInterface.java
+++
@@ -1,9 +0,0 @@
-
-public interface GameInterface {
-	
-	void createGameFrame(); // Create Frame
-	void createField(); // Create the Playing Field
-	void createStatement(); // Create the Statement Field
-	void createCheck();	// Create the Check Field. Contains the final gray squares and the check button
-	
-}
